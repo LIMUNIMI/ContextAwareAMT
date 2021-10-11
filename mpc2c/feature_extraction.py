Fixed variance computation

mpc2c/feature_extraction.py
@@ -416,6 +416,9 @@ class EncoderPerformer(LightningModule):
     def test_step(self, batch, batch_idx):
 
         pred = self.forward(batch['x'], batch['c'][0])
+        # TODO:
+        # * append latent variables to a list in this object
+        # * add test_epoch_end in which latent variables are clusterized
         return self.testloss(batch['y'], pred[:, 0]).cpu().numpy()
 
     def test_epoch_end(self, outputs, log=True):
@@ -465,9 +468,9 @@ class EncoderPerformer(LightningModule):
                         params[j] = params[j][perm_cols]
             # compute point-wise variances
             v = torch.var(torch.stack(params), dim=(0, ), unbiased=True)
-            # sum to the avg
-            s.append(v)
-        return utils.torch_moments(s)
+            # append to the list the average variance
+            s.append(torch.mean(v))
+        return utils.torch_moments(torch.stack(s))
 
     def losslog(self, name, value):
         self.log(name,

mpc2c/training.py
@@ -68,9 +68,9 @@ def model_test(model_build_func, test_sample):
 
 def cosine_distance(x, y, reduction='none'):
     out = 1 - F.cosine_similarity(x, y)
-    if reduction is 'none':
+    if reduction == 'none':
         return out
-    elif reduction is 'sum':
+    elif reduction == 'sum':
         return out.sum(axis=-1)
 
 
@@ -293,13 +293,12 @@ def train(hpar, mode, copy_checkpoint='', independence='specific', test=True):
         loss = ae_loss + perfm_loss
 
     logger.log_metrics({
-        "best_ae_val_loss":
-        float(ae_loss),  # type: ignore
-        "best_perfm_val_loss":
-        float(perfm_loss)  # type: ignore
+        "best_ae_val_loss": float(ae_loss),  # type: ignore
+        "best_perfm_val_loss": float(perfm_loss)  # type: ignore
     })
     logger.log_metrics({
-        "final_weight_variance_" + k: v for k, v in model.performer_weight_moments().items()
+        "final_weight_variance_" + k: v
+        for k, v in model.performer_weight_moments().items()
     })
 
     # this is the loss used by hyper-parameters optimization